9a946c10a03f1a470bde1d0fbf0cb4c814e94e51
homari25 <t09850mh@gmail.com>
2022-09-25 20:48:18 -0400

    added

--- projectTwo/scripts.js
+++ projectTwo/scripts.js
@@ -1,4 +1,4 @@
-let forbiddenCity,statueOfLiberty;
+let forbiddenCity,statueOfLiberty, backGround;
 let pixelLevel = 3;
 let pixelLevelTwo = 3;
 let inc = 0.02;
@@ -8,36 +8,53 @@ let n = 0.15;
 let d = 0.5;
 
 function preload(){
-  forbiddenCity = loadImage('forbiddencity.png');
+  backGround = loadImage("bg.png");
+  forbiddenCity = loadImage("forbiddencity.png");
   statueOfLiberty = loadImage("statueofliberty.png");
 }
 
 function setup() {
-  createCanvas(2048, 2048);
+  createCanvas(1200,1200);
   angleMode(DEGREES);
-  background(0);
+  // background(0);
   frameRate(120);
 }
 
 function draw(){
-
-
  pixelDensity(1);
- 
-
-
-drawStatueOfLiberty();
+ // drawbg();
+  drawStatueOfLiberty();
  drawForbiddenCity();
-//background(0);
-  // drawbg();
- 
 
 }
+
+// function drawbg() {
+//   image(backGround,0,0,800,800);
+//  loadPixels();
+//   for (let y = 0; y < height; y+=pixelLevel) {
+//     let xoff = 0;
+//     for (let x = 0; x <  width; x+=pixelLevel) {
+//     let k = (x + y * width) * 4
+//     let randomValue = random(250,255);
+// //  let n = noise(xoff,yoff) * 255;
+//     let blueValue = random(100,150);
+//     let r = 255 - pixels[k] ;
+//     let g = 200 - pixels[k + 1] ;
+//     let b = 150- pixels[k + 2] ;
+//     let a = pixels[k + 3] ;
+//     stroke();
+//      // // triangle(x-pixelLevel,y-10,x,y,x+10,y+10);
+//     // ellipse(x, y , pixelLevel);
+//       }
+//       }
+  
+  
+// }
 
 
 function drawStatueOfLiberty(){
   let yoff = 0;
- image(statueOfLiberty,width/2, height/2,600,700);
+ image(statueOfLiberty,width/2, 200,450,500);
  loadPixels();
   for (let y = 0; y < height; y+=pixelLevel) {
     let xoff = 0;
@@ -62,20 +79,21 @@ function drawStatueOfLiberty(){
 
 function drawForbiddenCity(){
     let yoffTwo = 0;
-  image(forbiddenCity,0,0,1280,720);
+  image(forbiddenCity,0,0,640,360);
+    loadPixels();
   for (let y = 0; y < forbiddenCity.height; y+=pixelLevelTwo) {
     let xoffTwo = 0;
-   loadPixels();
+ 
     for (let x = 0; x < forbiddenCity.width; x+=pixelLevelTwo) {
       let j = (x + y * width) * 4
-      let randomValueTwo = random(200,250);
-   //   let n = noise(xoff,yoff) * 255;
-      let redValue = random()*100;
-      let r2 = pixels[j + 0] = randomValueTwo;
-      let g2 = pixels[j + 1];
-      let b2 = pixels[j + 2] ;
-      let a2 = pixels[j + 3] ;
-      fill(r2, g2, 0, a2);
+    //  let randomValueTwo = random(200,250);
+     // let n = noise(xoffTwo,yoffTwo) * 255;
+      let redValue = random(100,150);
+      let r = pixels[j + 0] = redValue;
+      let g = pixels[j + 1];
+      let b = pixels[j + 2]  ;
+      let a = pixels[j + 3] ;
+      fill(r, 0, 0, a);
      // // triangle(x-pixelLevel,y-10,x,y,x+10,y+10);
      ellipse(x, y, pixelLevelTwo);
       }
