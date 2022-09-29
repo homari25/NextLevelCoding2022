f21636685ca1949b3e2146ee1a189ad4d09ed7cb
homari25 <t09850mh@gmail.com>
2022-09-29 18:00:48 -0400

    Update scripts.js

--- projectTwo/scripts.js
+++ projectTwo/scripts.js
@@ -14,59 +14,57 @@ function setup() {
 function draw() {
   usMap.loadPixels();
   image(usMap,0,0,width,height);
-  let diameter =10;
-  pixelDensity(1);
-//  background(255,0,0);
+  // let diameter =10;
+  // pixelDensity(1);
   
-  for (let y = 0; y < height; y++) {
-   for (let x = 0; x < width; x++) {
-    //  var pix = blue(usMap.get(x,y));
-    let index = (x + y * width) * 4;
-    let r = usMap.pixels [index + 0];
-    let g = usMap.pixels [index + 1];
-    let b = usMap.pixels [index + 2];
+  // for (let y = 0; y < height; y++) {
+  //  for (let x = 0; x < width; x++) {
+  //   let index = (x + y * width) * 4;
+  //   let r = usMap.pixels [index + 0];
+  //   let g = usMap.pixels [index + 1];
+  //   let b = usMap.pixels [index + 2];
 
-    let pix = ((0.3 * r) + (0.59* g) + (0.11 * b));
+  //   let pix = ((0.3 * r) + (0.59* g) + (0.11 * b));
 
-    if (pix <= 12.5 && x < usMap.width && y < usMap.height) {
-      noFill();
-    } else if (pix > 12.5&& pix <= 25) {
-      noFill();
-    } else if (pix > 25.0  && pix < 37.5) {
-      noFill();
-    } else if (pix > 37.5 && pix <= 50 ) {
-      noFill();
-    } else if (pix > 50 && pix <= 62.5 ) {
-      image(chinaFlag,x,y,10,10); 
-    } else if (pix > 62.5 && pix <= 75) {
-      image(chinaFlag,x,y,10,10);
-    } else if (pix > 75 && pix <= 87.5) {
-      image(chinaFlag,x,y,10,10); 
-    } else if (pix > 87.5 && pix <= 100) {
-      image(chinaFlag,x,y,10,10);
-    } else if (pix > 100 && pix <=112.5) {
-      image(chinaFlag,x,y,10,10); 
-    } else if (pix > 112.5 && pix <=125) {
-      image(usFlag,x,y,10,10); 
-    } else if (pix > 125 && pix <=137.5) {
-      image(usFlag,x,y,10,10); 
-    } else if (pix > 137.5 && pix <=150) {
-      image(usFlag,x,y,10,10); 
-    } else if (pix > 150 && pix <=162.5) {
-      image(usFlag,x,y,10,10); 
-    } else if (pix > 162.5 && pix <=175) {
-      image(usFlag,x,y,10,10); 
-    } else if (pix > 175 && pix <=187.5) {
-      image(usFlag,x,y,10,10); 
-    } else if (pix > 187.5 && pix <200) {
-      image(usFlag,x,y,10,10); 
-    } else if (pix > 187.5 && pix <200) {
-      image(usFlag,x,y,10,10); 
-    } else if (pix > 187.5 && pix <200) {
-      image(usFlag,x,y,10,10); 
-    }
-    x = x + diameter - 0.5;
-  }
-    y = y + diameter - 0.5;
-  }
+  //   if (pix <= 12.5 && x < usMap.width && y < usMap.height) {
+  //     noFill();
+  //   } else if (pix > 12.5&& pix <= 25) {
+  //     noFill();
+  //   } else if (pix > 25.0  && pix < 37.5) {
+  //     noFill();
+  //   } else if (pix > 37.5 && pix <= 50 ) {
+  //     noFill();
+  //   } else if (pix > 50 && pix <= 62.5 ) {
+  //     image(chinaFlag,x,y,10,10); 
+  //   } else if (pix > 62.5 && pix <= 75) {
+  //     image(chinaFlag,x,y,10,10);
+  //   } else if (pix > 75 && pix <= 87.5) {
+  //     image(chinaFlag,x,y,10,10); 
+  //   } else if (pix > 87.5 && pix <= 100) {
+  //     image(chinaFlag,x,y,10,10);
+  //   } else if (pix > 100 && pix <=112.5) {
+  //     image(chinaFlag,x,y,10,10); 
+  //   } else if (pix > 112.5 && pix <=125) {
+  //     image(usFlag,x,y,10,10); 
+  //   } else if (pix > 125 && pix <=137.5) {
+  //     image(usFlag,x,y,10,10); 
+  //   } else if (pix > 137.5 && pix <=150) {
+  //     image(usFlag,x,y,10,10); 
+  //   } else if (pix > 150 && pix <=162.5) {
+  //     image(usFlag,x,y,10,10); 
+  //   } else if (pix > 162.5 && pix <=175) {
+  //     image(usFlag,x,y,10,10); 
+  //   } else if (pix > 175 && pix <=187.5) {
+  //     image(usFlag,x,y,10,10); 
+  //   } else if (pix > 187.5 && pix <200) {
+  //     image(usFlag,x,y,10,10); 
+  //   } else if (pix > 187.5 && pix <200) {
+  //     image(usFlag,x,y,10,10); 
+  //   } else if (pix > 187.5 && pix <200) {
+  //     image(usFlag,x,y,10,10); 
+  //   }
+  //   x = x + diameter - 0.5;
+  // }
+  //   y = y + diameter - 0.5;
+  // }
 }
