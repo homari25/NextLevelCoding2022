655a52b515105d45172a3d8e3cfeb6765229b87b
homari25 <t09850mh@gmail.com>
2022-10-02 13:23:25 -0400

    Update scripts.js

--- projectTwo/scripts.js
+++ projectTwo/scripts.js
@@ -78,6 +78,12 @@ function draw() {
     } else if (pix > 312.5 && pix <= 325) {
       image(nofill,x,y,10,10);
     }
+
+    if (mouseover) {
+    if (image == "usFlag")
+      image(chinaFlag,x,y,10,10);
+    }
+    
     x = x + diameter - 1;
   }
     y = y + diameter - 1;
