20380158ef6db52d433bc7db22124ea1a2b9747a
homari25 <t09850mh@gmail.com>
2022-10-02 18:25:28 -0400

    Update scriptsTwo.js

--- projectTwo/scriptsTwo.js
+++ projectTwo/scriptsTwo.js
@@ -1,22 +1,22 @@
-let usMap,chinaFlag,usFlag,nofill;
+// let usMap,chinaFlag,usFlag,nofill;
 
-function preload(){
-  usMap = loadImage("map.png");
-  chinaFlag = loadImage("china.png");
-  usFlag = loadImage("us.png");
-  nofill = loadImage("nofill.png")
-}
+// function preload(){
+//   usMap = loadImage("map.png");
+//   chinaFlag = loadImage("china.png");
+//   usFlag = loadImage("us.png");
+//   nofill = loadImage("nofill.png")
+// }
 
-function setup() {
-  createCanvas(2048, 2048);
-//  noLoop();
-}
+// function setup() {
+//   createCanvas(2048, 2048);
+// //  noLoop();
+// }
 
-function draw() {
-  for (let y = 0; y < height; y++) {
-   for (let x = 0; x < width; x++) {
-        image(nofill,x,y,10,10); 
-        image(nofill,x,y,10,10); 
-    }
-  }
-}
+// function draw() {
+//   for (let y = 0; y < height; y++) {
+//    for (let x = 0; x < width; x++) {
+//         image(nofill,x,y,10,10); 
+//         image(nofill,x,y,10,10); 
+//     }
+//   }
+// }
