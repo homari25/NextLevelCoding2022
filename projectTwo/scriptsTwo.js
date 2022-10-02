106a2386c7c752f74cc07f5e02aae5d763c0369e
homari25 <t09850mh@gmail.com>
2022-10-02 18:22:16 -0400

    Update scriptsTwo.js

--- projectTwo/scriptsTwo.js
+++ projectTwo/scriptsTwo.js
@@ -13,24 +13,10 @@ function setup() {
 }
 
 function draw() {
-  image(usMap,0,0,usMap.width,usMap.height);
-  usMap.loadPixels();
-  let diameter = 10;
-  pixelDensity(1);
-  console.log(mouseX,mouseY);
-
   for (let y = 0; y < height; y++) {
    for (let x = 0; x < width; x++) {
-    let index = (x + y * width) * 4;
-    let r = usMap.pixels[index + 0];
-    let g = usMap.pixels[index + 1];
-    let b = usMap.pixels[index + 2];
-
-    let pix = ((0.08 * r) + (0.54* g) + (0.11 * b));
-
-      if (pix <= 12.5) {
         image(nofill,x,y,10,10); 
-      } else if (pix > 12.5&& pix <= 25) {
         image(nofill,x,y,10,10); 
-    
+    }
+  }
 }
