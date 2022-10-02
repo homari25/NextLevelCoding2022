7baa2774b60f54222c221d7f297bf381449ecd7a
homari25 <t09850mh@gmail.com>
2022-10-02 11:57:46 -0400

    Update scripts.js

--- projectTwo/scripts.js
+++ projectTwo/scripts.js
@@ -36,7 +36,7 @@ function draw() {
     } else if (pix > 37.5 && pix <= 50 ) {
       image(nofill,x,y,10,10); 
     } else if (pix > 50 && pix <= 62.5 ) {
-      image(chinaFlag,x,y,10,10); 
+      image(nofill,x,y,10,10); 
     } else if (pix > 62.5 && pix <= 75) {
       image(usFlag,x,y,10,10);
     } else if (pix > 75 && pix <= 87.5) {
@@ -64,7 +64,7 @@ function draw() {
     } else if (pix > 225 && pix <= 237.5) {
       image(chinaFlag,x,y,10,10); 
     } else if (pix > 237.5 && pix <=250) {
-      image(nofill,x,y,10,10); 
+      image(chinaFlag,x,y,10,10); 
     } else if (pix > 250 && pix <= 255) {
       image(nofill,x,y,10,10); 
     } else if (pix > 255 && pix <= 275 ) {
