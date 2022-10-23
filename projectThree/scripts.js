e89643f41d1f7fbdc4d738d22975e98d917bc599
homari25 <t09850mh@gmail.com>
2022-10-23 15:27:42 -0400

    project three uploaded

--- projectThree/scripts.js
+++ projectThree/scripts.js
@@ -3,77 +3,84 @@ let frame = document.getElementById("frame");
 let nal = document.getElementById("nal");
 let ge = document.getElementById("ge");
 let graphic = document.getElementById("graphic");
-// let geoframe = document.getElementById("geoframe");
+let sunrise,sunset,sunlength;
+let widthChange,heightChange;
+let lat,lng,place;
+let dateInput = document.getElementById("date");
+let Input = document.getElementById("latitude");
+let longtitudeInput = document.getElementById("longtitude");
+let placeInput = document.getElementById("place");
+// let mysvg = document.getElementById("mysvg");
+
+function submit(){
+console.log(placeInput.value);
+place = placeInput.value;
+date = dateInput.value;
 
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '771845e547msh3cf2aeb936308e7p110648jsn9a26e17e8605',
-		'X-RapidAPI-Host': 'stormglass.p.rapidapi.com'
+		'X-RapidAPI-Key':'771845e547msh3cf2aeb936308e7p110648jsn9a26e17e8605',
+		'X-RapidAPI-Host': 'forward-reverse-geocoding.p.rapidapi.com'
 	}
 };
 
-fetch('https://stormglass.p.rapidapi.com/forecast?lng=60&lat=5.114', options)
+fetch(`https://forward-reverse-geocoding.p.rapidapi.com/v1/search?q=${place}&accept-language=en&polygon_threshold=0.0`, options)
 	.then(response => response.json())
 	.then(response => {
-      console.log(response)
-      console.log(response.hours[0].waveHeight[0].value)
-      myVar = response.hours[0].waveHeight[0].value; //If I want to get a value. Check from sources!!
-  
-      console.log(myVar);
-  
-  })
-	.catch(err => console.error(err));
+  console.log(response[0].lat,response[0].lon);
+  lat = response[0].lat;
+  lng = response[0].lon;
 
-let heightChange = 10;
-let widthChange = 10;
-let moveLetter = widthChange - 10;
-let moveVertical = heightChange - 10;
+const optionsTwo = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '771845e547msh3cf2aeb936308e7p110648jsn9a26e17e8605',
+		'X-RapidAPI-Host': 'sun-calculator.p.rapidapi.com'
+	}
+};
 
-document.addEventListener('keydown',function(e) {
-  if (e.which === 39) {
-  widthChange = widthChange + 10;  
-  moveLetter = widthChange;
-  // console.log(moveLetter);
-  } else if (e.which === 37) {
-  widthChange = widthChange - 10;  
-  moveLetter = widthChange;
-  } else if (e.which === 38) {
-    heightChange = heightChange - 10;
-    moveVertical = heightChange;
-  } else if (e.which === 40) {
-    heightChange = heightChange + 10;
-    moveVertical = heightChange;
+fetch(`https://sun-calculator.p.rapidapi.com/sunrise/?date=${date}&lat=${lat}&lng=${lng}`, optionsTwo)
+	.then(response => response.json())
+	.then(response => {
+   console.log(response.sunrise, response.sunset);
+   let sunrise = response.sunrise;
+   let sunset = response.sunset;
+ 
+  let sunlength = (sunset - sunrise)/3600;
+  console.log(sunlength);
+ 
+
+// //I can't map.....
+// // let widthChange = map(sunlength,5,16,100,500); 
+  widthChange = sunlength * 10; 
+  if (sunlength < 11) {
+    heightChange = 10;
+  }  else if (sunlength < 13) {
+    heightChange = 30;
+  } else if (sunlength <15) {
+    heightChange = 60;
+  } else if (sunlength > 15) {
+    heightChange = 100;
   }
-  // console.log(moveLetter);
+
   frame.style.width = widthChange + "px";
   frame.style.height = 84 + heightChange + "px";
   nal.style.left = 250 + widthChange + "px";
   ge.style.top = 160 + heightChange + "px";
   graphic.style.top = 160 + heightChange + "px";
   graphic.style.left = 250 + widthChange + "px";
-  
+
 })
+  .catch(err => console.error(err));
 
-console.log(widthChange);
+})
+	.catch(err => console.error(err));
+}
 
-let myVar;
-
-// frame.style.width = widthChange + "px";
-// //geoframe.style.width = widthChange + "px";
-
-// nal.style.left = 260 + moveLetters + "px";
-// graphic.style.left = 260 + moveLetters + "px";
-
-
-
-// const lat = 60;
-// const lng = 5.114;
-
-// fetch(`https://api.stormglass.io/v2/tide/extremes/point?lat=${lat}&lng=${lng}&start=2019-03-15&end=2019-03-15`, {
-//   headers: {
-//     'Authorization': 'acba17b8-4dd2-11ed-92e6-0242ac130002-acba1812-4dd2-11ed-92e6-0242ac130002'
-//   }
-// }).then(response => response.json()).then((jsonData) => {
-//   console.log(response.json()); 
-// });
+// function save(){
+//      var link = document.createElement("a");
+//     link.download = 'natgeo-logo.png';
+//     link.href = document.getElementById('mysvg');
+//     link.click();
+// }
