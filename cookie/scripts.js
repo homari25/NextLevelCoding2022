4c5deb8796a9c720204bc656a95ea19a2fbd8396
homari25 <t09850mh@gmail.com>
2022-10-30 14:51:18 -0400

    Update scripts.js

--- cookie/scripts.js
+++ cookie/scripts.js
@@ -10,7 +10,6 @@ let newPic = document.createElement("div");
 let i = 0;
 if ( i < myPics.length) {
   newPic.innerHTML = "<img src='" + myPics[i] + "'>";
-  i = i + 1;
 }
 
 newPic.classList.add("newyorkPic");
@@ -22,5 +21,6 @@ newPic.style.left = Math.random() * 500 + "px";
   
 document.body.appendChild(newPic);
 console.log(i);
+i = i + 1;
 
 }
