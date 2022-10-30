ce90c116b05c7180d4c29ff96fc8f13253db5516
homari25 <t09850mh@gmail.com>
2022-10-30 14:43:19 -0400

    adjust size

--- cookie/scripts.js
+++ cookie/scripts.js
@@ -9,7 +9,8 @@ function myFunction () {
 let newPic = document.createElement("div");
 newPic.innerHTML = "<img src='" + myPics[Math.floor(Math.random() * myPics.length)] + "'>";
 newPic.classList.add("newyorkPic");
-newyorkPic.style.width = 50 + "px";
+newPic.style.width = 50 + "px";
+// how do I make it small?
 newPic.style.position = "absolute";
 newPic.style.top = Math.random() * 500 + "px";
 newPic.style.left = Math.random() * 500 + "px";
