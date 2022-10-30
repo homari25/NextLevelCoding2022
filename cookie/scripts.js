4fc195dda5ebf120ce409a2885f86fe2610458a2
homari25 <t09850mh@gmail.com>
2022-10-30 11:46:49 -0400

    adding files

--- cookie/scripts.js
+++ cookie/scripts.js
@@ -1,74 +1,14 @@
-let quiz = document.getElementById("quiz");
-let score = document.getElementById("score");
-let time = document.getElementById("time");
-let scoreCount = 0;
-let timeCount = 20;
-let quizFood = ['okonomiyaki','takoyaki','care-raisu','sushi','omuraisu','hitsumabushi','monjayaki','nattou','tempura','sukiyaki','misoshiru','kaisendon'];
-let textInput = document.getElementById("textbox");
-let enter = document.getElementById("enter");
-let message = document.getElementById('message');
-let picture = document.getElementById("picture");
-let myPics = ["okonomiyaki.jpg","takoyaki.jpg","care-raisu.jpg","sushi.jpg","omuraisu.jpg","hitsumabushi.jpeg","monjayaki.jpeg","nattou.jpeg","tempura.jpeg","sukiyaki.jpg","misoshiru.jpg","kaisendon.jpg"];
+let image = document.getElementById("image");
 
+let myPics = ["https://media.cntraveler.com/photos/611fc0aced1e8a109123ec48/master/w_3000,h_2001,c_limit/Broadway,-New-York_GettyImages-588653038.jpg", "https://blog-www.pods.com/wp-content/uploads/2019/04/MG_1_1_New_York_City-1.jpg", "https://media.tacdn.com/media/attractions-content--1x-1/0b/2d/dc/c2.jpg", "https://media-cdn.tripadvisor.com/media/photo-m/1280/25/14/2e/02/caption.jpg"];
 
-// picture.innerHTML = "<img src='" + myPics[Math.floor(Math.random() * myPics.length)] + "'>";
-textbox.innerHTML = textInput.value;
-let i = 0;
+document.addEventListener("click",myFunction);
 
-enter.onclick = function(){ 
-
-  quiz.innerHTML = quizFood[i];
-
-  if (quizFood[i - 1] == textInput.value ) {
-    scoreCount+=1;
-    message.innerHTML = "Yay! :)";
-
-  } else {
-      scoreCount +=0;
-      message.innerHTML = "";
-  }
-  console.log(myPics[i])
-  // myPics[i].style.width = 200 + "px";
-  // myPics[i].style.height = 200 + "px";
-  picture.innerHTML = "<img src='" + myPics[i] + "'>" ;
-
-  quiz.innerHTML = quizFood[i];
-
-  i++;
-  // let answer = Math.floor(Math.random() * quizFood.length);
-
-  // console.log(quizFood[answer] + "," + textInput.value)
-  // quiz.innerHTML = quizFood[answer];
-
-  // if (quizFood[answer] == textInput.value ) {
-  //   scoreCount+=1;
-  //   message.innerHTML = "CORRECT!";
-  // } else {
-  //     scoreCount +=0;
-  // }
-
-  // quiz.innerHTML = quizFood[answer];
-  score.innerHTML = "SCORE " + scoreCount;
-  textInput.value = "";
+function myFunction () {
+image.innerHTML = "<img src='" + myPics[Math.floor(Math.random() * myPics.length)] + "'>";
+image.style.height = 10 + "%"; 
 }
 
-// Pick & show random word
-
-
-//time goes down
-setInterval(timer,1000);
-
-function timer(){
-time.innerHTML = "TIME " + timeCount;
-
-
-
-if (timeCount > 0) {
-  timeCount--;
-} else {
-  timeCount == 0;
-//when time is up 
-  message.innerHTML= "GAME OVER!!";
-}
-}
-
+// var img = document.createElement('img')
+// img.src = 'images/aftersurprise.png';
+// document.getElementById('surprise').appendChild(img);
