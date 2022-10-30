ce6a906eee407543010a4c5ba628c0da56c61c56
homari25 <t09850mh@gmail.com>
2022-10-30 15:01:32 -0400

    Update scripts.js

--- cookie/scripts.js
+++ cookie/scripts.js
@@ -8,14 +8,54 @@ text.addEventListener("click",myFunction);
 function myFunction () {
 let newPic = document.createElement("div");
 let i = 0;
+//how do I not repeat the same thing over again?
 newPic.innerHTML = "<img src='" + myPics[Math.floor(Math.random() * myPics.length)] + "'>";
 
 newPic.classList.add("newyorkPic");
-newPic.style.width = 50 + "px";
 // how do I make it small?
+newPic.style.width = 50 + "px";
 newPic.style.position = "absolute";
-newPic.style.top = Math.random() * 500 + "px";
-newPic.style.left = Math.random() * 500 + "px";
+
+dragElement(document.getElementsByClassName("newyorkPic"));
+
+function dragElement(elmnt) {
+  let pos1, pos2, pos3, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    elmnt.onmousedown = dragMouseDown;
+  }  
+}
+
+function dragMouseDown(e) {
+  e = e || window.event;
+  e.preventDefault();
+  // get the mouse cursor position at startup:
+  pos3 = e.clientX;
+  pos4 = e.clientY;
+  document.onmouseup = closeDragElement;
+  // call a function whenever the cursor moves:
+  document.onmousemove = elementDrag;
+}
+
+function elementDrag(e) {
+  e = e || window.event;
+  e.preventDefault();
+  pos1 = pos3 - e.clientX;
+  pos2 = pos4 - e.clientY;
+  pos3 = e.clientX;
+  pos4 = e.clientY;
+  elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+  elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+}
+
+function closeDragElement() {
+  document.onmouseup = null;
+  document.onmousemove = null;
+}
+
+// newPic.style.top = Math.random() * 500 + "px";
+// newPic.style.left = Math.random() * 500 + "px";
   
 document.body.appendChild(newPic);
 console.log(i);
