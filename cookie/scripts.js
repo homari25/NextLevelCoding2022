d39a8d9e448a2ba76ed45c4916183d287ca7b07a
homari25 <t09850mh@gmail.com>
2022-10-30 14:16:23 -0400

    testing

--- cookie/scripts.js
+++ cookie/scripts.js
@@ -1,14 +1,19 @@
-let image = document.getElementById("image");
+let circle = document.getElementById("circle");
 
-let myPics = ["https://media.cntraveler.com/photos/611fc0aced1e8a109123ec48/master/w_3000,h_2001,c_limit/Broadway,-New-York_GettyImages-588653038.jpg", "https://blog-www.pods.com/wp-content/uploads/2019/04/MG_1_1_New_York_City-1.jpg", "https://media.tacdn.com/media/attractions-content--1x-1/0b/2d/dc/c2.jpg", "https://media-cdn.tripadvisor.com/media/photo-m/1280/25/14/2e/02/caption.jpg"];
-
-document.addEventListener("click",myFunction);
+circle.addEventListener("click",myFunction);
 
 function myFunction () {
-image.innerHTML = "<img src='" + myPics[Math.floor(Math.random() * myPics.length)] + "'>";
-image.style.height = 10 + "%"; 
-}
+let newCircle = document.createElement("div");
 
-// var img = document.createElement('img')
-// img.src = 'images/aftersurprise.png';
-// document.getElementById('surprise').appendChild(img);
+newCircle.style.width = 100 + "px";
+newCircle.style.height = 100 + "px";
+newCircle.classList.add("newCircles");
+newCircle.style.position = "absolute"
+newCircle.style.backgroundColor = "blue";
+newCircle.style.borderRadius = 50 + "%";
+newCircle.style.top = Math.random() * 500 + "px";
+newCircle.style.left = Math.random() * 500 + "px";
+  
+document.body.appendChild(newCircle);
+console.log(newCircle);
+}
