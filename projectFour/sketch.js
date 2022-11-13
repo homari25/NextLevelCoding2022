5abf299672afae5d2b0e3ac18007eb2892f339fb
homari25 <t09850mh@gmail.com>
2022-11-13 15:22:34 -0500

    Update sketch.js

--- projectFour/sketch.js
+++ projectFour/sketch.js
@@ -55,10 +55,10 @@ function draw() {
   for (let i = count; i > 0; i--) {
   push();
   translate(100,180);
-  fill(255,255,255,20);
-  ellipse(0,0,90,90);
   rotate(TWO_PI/count/2 * i);
   drawFlower();
+  fill(255,255,255,20);
+  ellipse(0,0,60,60);
   pop();
 
   push();
