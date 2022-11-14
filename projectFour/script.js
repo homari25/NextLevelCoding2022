fefbf4791eada82b0618eec0233a3c3159935bcd
homari25 <t09850mh@gmail.com>
2022-11-14 09:22:29 -0500

    Update script.js

--- projectFour/script.js
+++ projectFour/script.js
@@ -27,7 +27,8 @@ fetch(opensheet_url)
         for (let i = 0; i < lushname.length; i++) {
             // if(item[itemNum] == lushname[itemNum]) {
         if(soapNameInput.value == items) {
-        console.log(items,sweetness);
+        count = map(sweetness,1,10,10,60);
+        console.log(items,sweetness,count);
 				//do something with the data here
                 // }
             }
