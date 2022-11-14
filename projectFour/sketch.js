8ca2ad3728f62b3b52c5b9b11046193d3e90cee1
homari25 <t09850mh@gmail.com>
2022-11-14 08:22:51 -0500

    Update sketch.js

--- projectFour/sketch.js
+++ projectFour/sketch.js
@@ -12,7 +12,7 @@ let amplitude = 70.0; // Height of wave
 let period = 500.0; // How many pixels before the wave repeats
 let dx; // Value for incrementing x
 let yvalues; // Using an array to store height values for the wave
-let offset = 0.05;
+let offset = Math.random() / 100;
 //color
 let c1 = "rgb(255,125,0)";
 let c2 = "rgb(0,125,125)"
@@ -157,7 +157,6 @@ function calcWave() {
   for (let i = 0; i < yvalues.length; i++) {
     yvalues[i] = sin(x) * refresh * offset;
     x += dx;
-    offset += offset
   }
 }
 
