6e4cead229bb8cc2b1b40cbcf4f8844a6de0a5e9
homari25 <t09850mh@gmail.com>
2022-11-14 19:46:45 -0500

    Update sketch.js

--- projectFour/sketch.js
+++ projectFour/sketch.js
@@ -22,7 +22,7 @@ let flowerChanger = document.getElementById("flowerChanger");
 //flower
 // let randomX = random(100,400);
 // let randomY = random(100,400);
-let petalSize = random(100,400);
+let petalSize = Math.random() * 400 - 100;
 let flowerSize = petalSize - 60;
 
 function setup() {
