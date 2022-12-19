c2d72cdfd2bacbbaac664403b639ea302fee042e
homari25 <t09850mh@gmail.com>
2022-12-18 23:59:00 -0500

    rebuilding

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// let body = document.querySelectorAll("body")[0];
+let body = document.querySelectorAll("body")[0];
 // let weeknum = document.querySelectorAll("weeknum")[0];
 
 // function changeColoring() {
@@ -12,5 +12,63 @@
 
 // }
 
-let description = document.querySelectorAll(".description");
 let projectTwo = document.querySelector(".projectTwo");
+let projectTwoPres = document.querySelector(".projectTwoPres");
+let projectTwoDig = document.querySelector(".projectTwoDig");
+let projectTwoArt = document.querySelector(".projectTwoArt");
+let counterTwo = 0;
+
+let projectThree = document.querySelector(".projectThree");
+let projectThreePres = document.querySelector(".projectThreePres");
+let projectThreeGen = document.querySelector(".projectThreeGen");
+let counterThree = 0;
+
+let projectFour = document.querySelector(".projectFour");
+let projectFourPres = document.querySelector(".projectFourPres");
+let projectFourGen = document.querySelector(".projectFourGen");
+let counterFour = 0;
+
+let description = document.querySelectorAll(".description");
+
+projectTwo.onclick = function () {
+  counterTwo +=1;
+
+  if (counterTwo %2 == 1) {
+    projectTwoPres.style.display = "block";
+    projectTwoDig.style.display = "block";
+    projectTwoArt.style.display = "block";  
+  } else {
+    projectTwoPres.style.display = "none";
+    projectTwoDig.style.display = "none";
+    projectTwoArt.style.display = "none";  
+  }
+
+}
+
+
+projectThree.onclick = function () {
+  counterThree +=1;
+
+  if (counterThree %2 == 1) {
+    projectThreePres.style.display = "block";
+    projectThreeGen.style.display = "block";
+  } else {
+    projectThreePres.style.display = "none";
+    projectThreeGen.style.display = "none";
+  }
+
+}
+
+projectFour.onclick = function () {
+  counterFour +=1;
+
+  if (counterFour %2 == 1) {
+    projectFourPres.style.display = "block";
+    projectFourGen.style.display = "block";
+  } else {
+    projectFourPres.style.display = "none";
+    projectFourGen.style.display = "none";
+  }
+
+}
+
