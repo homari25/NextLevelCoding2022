3b64ddcad6f03cd8ab6e80dea4643ac62c520f98
homari25 <t09850mh@gmail.com>
2022-12-18 23:31:37 -0500

    rebuilding website

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
-let body = document.querySelectorAll("body")[0];
-let weeknum = document.querySelectorAll("weeknum")[0];
+// let body = document.querySelectorAll("body")[0];
+// let weeknum = document.querySelectorAll("weeknum")[0];
 
-function changeColoring() {
+// function changeColoring() {
  
-  let myColors = ["#E8A365", "#E78A66", "#D06E6E","#81B29A","#F2CC8F","#3D405B"]
-  body.style.backgroundColor = myColors[Math.floor(Math.random() * myColors.length)];
-  weeknum.style.color = myColors[Math.floor(Math.random() * myColors.length)];
+//   let myColors = ["#E8A365", "#E78A66", "#D06E6E","#81B29A","#F2CC8F","#3D405B"]
+//   body.style.backgroundColor = myColors[Math.floor(Math.random() * myColors.length)];
+//   weeknum.style.color = myColors[Math.floor(Math.random() * myColors.length)];
 
 
-// console.log(myColors[Math.floor(Math.random() * myColors.length)])
+// // console.log(myColors[Math.floor(Math.random() * myColors.length)])
 
-}
+// }
+
+let description = document.querySelectorAll(".description");
+let projectTwo = document.querySelector(".projectTwo");
